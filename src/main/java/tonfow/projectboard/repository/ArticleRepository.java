6a696e42fca557312bacd823d47f48123dbea4fc
Limjuhan <starlexy@naver.com>
2025-05-04 12:27:49 +0900

    SpringDataRest + HAL 추가 api테스트 완료

--- src/main/java/tonfow/projectboard/repository/ArticleRepository.java
+++ src/main/java/tonfow/projectboard/repository/ArticleRepository.java
@@ -1,7 +1,9 @@
 package tonfow.projectboard.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import tonfow.projectboard.domain.Article;
 
+@RepositoryRestResource
 public interface ArticleRepository extends JpaRepository<Article, Long> {
 }
